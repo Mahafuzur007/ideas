123473291110942b3b5e183307a37df034c03d98
Mahafuzur007 <mahafuzurrahmanhridoy@gmail.com>
2023-09-21 01:20:13 +0600

    Blade templates & Layouts

---
+++ resources/views/terms.blade.php
@@ -0,0 +1,31 @@
+@extends('layout.master')
+
+@section('content')
+
+<body>
+    <div class="container py-4">
+        <h1>Terms</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, alias vel neque quisquam ad, laborum ut
+            distinctio, hic ullam adipisci sint temporibus dolore dolor. Fugit doloribus enim itaque recusandae sapiente
+            praesentium, quis unde nisi animi quasi dicta doloremque laudantium qui dolorem molestias alias sunt officia
+            et.
+            Repudiandae ab sed neque esse dolorum aperiam ipsum quos hic possimus ipsam consequatur enim, quam laborum
+            accusantium voluptates aliquam molestiae numquam nobis soluta magnam incidunt quaerat cumque eius debitis.
+            Quos
+            debitis odit fugiat obcaecati, voluptates tenetur perspiciatis, provident amet labore tempora, dolor dicta
+            molestiae earum architecto? Amet nam dolorum sunt? Voluptas rem qui nemo praesentium dicta assumenda
+            accusamus
+            aliquid eligendi, possimus repudiandae provident maiores dolor expedita consectetur cumque eaque incidunt
+            pariatur sit placeat perspiciatis sequi nostrum similique perferendis facilis! Libero provident ipsa beatae,
+            unde perspiciatis velit excepturi repudiandae repellendus quia optio neque tenetur? Et iste dolor fuga
+            facilis
+            voluptatum ad sit architecto neque exercitationem natus impedit aperiam eos, ipsa odit molestias nihil
+            officia.
+            Repellat explicabo eum voluptatibus temporibus beatae, hic totam minima possimus qui nesciunt blanditiis quo
+            maxime inventore illum aliquam impedit labore magni ipsum placeat corporis doloremque ipsam nam officiis.
+            Quis
+            iusto, voluptatibus itaque reiciendis, eaque repudiandae, pariatur beatae molestias reprehenderit voluptatum
+            temporibus?</p>
+    </div>
+</body>
+@endsection
